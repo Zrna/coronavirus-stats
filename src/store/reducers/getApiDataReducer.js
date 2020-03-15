1f7965e0e596a3671bce9079506c4dff249a1498
Zrna <lukazrnic5@gmail.com>
2020-03-15 23:28:10 +0100

    Currently sick store property added

--- src/store/reducers/getApiDataReducer.js
+++ src/store/reducers/getApiDataReducer.js
@@ -11,6 +11,7 @@ const getApiDataReducer = (state = {}, action) => {
       numberOfConfirmed: action.data[0].latest.confirmed,
       numberOfDeaths: action.data[0].latest.deaths,
       numberOfRecovered: action.data[0].latest.recovered,
+      numberOfCurrentlySick: (action.data[0].latest.confirmed - action.data[0].latest.recovered),
       lastUpdated: action.data[0].confirmed.last_updated,
       countryConfirmedDataSum: action.data[1][0].countryConfirmedDataSum,
       countryDeathsDataSum: action.data[1][0].countryDeathsDataSum,
