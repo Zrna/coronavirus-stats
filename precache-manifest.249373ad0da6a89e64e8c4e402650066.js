5ee02805043fa1a1c8450ef616d4f3bf5d182bd9
Zrna <lukazrnic5@gmail.com>
2020-03-21 18:12:03 +0100

    Updates

---
+++ precache-manifest.249373ad0da6a89e64e8c4e402650066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579742d6f14d0701bc7cf87d7d6ee3ab",
+    "url": "/coronavirus-stats/index.html"
+  },
+  {
+    "revision": "a415f06a245bfa1cdd4a",
+    "url": "/coronavirus-stats/static/css/main.8be292a6.chunk.css"
+  },
+  {
+    "revision": "8caeaf28a9ecc1b1021f",
+    "url": "/coronavirus-stats/static/js/2.b9625dae.chunk.js"
+  },
+  {
+    "revision": "7f7b7554ad8c1d8206ae278b825bcae0",
+    "url": "/coronavirus-stats/static/js/2.b9625dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a415f06a245bfa1cdd4a",
+    "url": "/coronavirus-stats/static/js/main.18c642b7.chunk.js"
+  },
+  {
+    "revision": "b636feff4b3b1e4a72ae",
+    "url": "/coronavirus-stats/static/js/runtime-main.77797276.js"
+  }
+]);
